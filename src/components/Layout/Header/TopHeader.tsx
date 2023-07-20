80ba9e83a7e705d223c2c6ce0c07e14a18791a41
RAYAN ALHERAKI <rayyanalhiraki3@gmail.com>
2023-07-20 22:17:09 +0300

    make header responsive

--- src/components/Layout/Header/TopHeader.tsx
+++ src/components/Layout/Header/TopHeader.tsx
@@ -1,13 +1,22 @@
 import clsx from "clsx";
 import styles from "./Header.module.css";
 import { MdSearch } from "react-icons/md";
+import { BsList } from "react-icons/bs";
 
 export default function TopHeader() {
   return (
     <div className={clsx(styles["top-header"], "container mx-auto")}>
+      <div className={styles["show-in-small-screen"]}>
+        <BsList style={{ fontSize: "32px", color: "var(--secondary-main)" }} />
+      </div>
       <div className={styles.logo}>
         <span style={{ color: "var(--primary-main)" }}>LO</span>
         <span style={{ color: "var(--secondary-main)" }}>GO</span>
+      </div>
+      <div className={styles["show-in-small-screen"]}>
+        <MdSearch
+          style={{ fontSize: "32px", color: "var(--secondary-main)" }}
+        />
       </div>
       <div className={styles["search-box"]}>
         <MdSearch style={{ fontSize: "22px" }} />
